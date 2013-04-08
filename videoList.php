d7b161a442b08ec9117cbd959e096266da18b4e3
REY ORTEGA <LTAD@reys-mac-pro.local.valenciacc.edu>
2013-04-08 09:21:11 -0400

    Add notes and remove isDot() as the strpos already checks for dot at string position zero

--- videoList.php
+++ videoList.php
@@ -2,24 +2,29 @@
 
 class videoList extends DirectoryIterator
 {
-
-	private $files = array();
-
+	// Protect the array list from being changed publicly
+	protected $files = array();
+	
+	// Construct that iterates through file level directory and stores list into protected array
 	public function __construct()
 	{
 		
+		// Call the DirectoryIterator Class
 		$iterator = new DirectoryIterator(dirname(__FILE__));
-
+		
+		// Pass DirectoryIterator objects into working variable $fileinfo
 		foreach($iterator as $fileinfo)
 		{
-			if($fileinfo->isFile() && !$fileinfo->isDot() && strpos($fileinfo, ".")!= 0)
+			// Only store files in protected array that do not contain dots at first position of string
+			if($fileinfo->isFile() && strpos($fileinfo, ".")!= 0)
 			{
-
+				// Store result of condition in protected array
 				$this->files[] = $fileinfo->getFilename();
 			}
 		}
 	}
-
+	
+	// Function to publicly access protected array data
 	public function getFiles()
 	{
 		return $this->files;
@@ -28,6 +33,8 @@ class videoList extends DirectoryIterator
 
 }
 
+
+// Test class and method
 $f = new VideoList();
 foreach($f->getFiles() as $x)
 {
